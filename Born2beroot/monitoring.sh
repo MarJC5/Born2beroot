6b3245ace2df7bb336629b4020bea32cc0e260b3
MarJC5 <jmartin@student.42lausanne.ch>
2021-11-17 17:39:47 +0100

    Update

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -6,8 +6,22 @@
 #    By: jmartin <marvin@42lausanne.ch>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/15 11:54:27 by jmartin           #+#    #+#              #
-#    Updated: 2021/11/15 11:54:34 by jmartin          ###   ########.fr        #
+#    Updated: 2021/11/17 17:39:24 by jmartin          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+#!/bin/bash
 
+wall
+'#Architecture: ' `uname -a` \
+'\n#CPU physical: ' \
+'\n#vCPU: ' \
+'\n#Memory Usage: ' \
+'\n#Disk Usage: ' \
+'\n#CPU load: ' \
+'\n#Last boot: ' \
+'\n#LVM use: ' \
+'\n#Connexions TCP: ' \
+'\n#User log: ' \
+'\n#Network: IP' `hostname -I` `sudo ifconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'` \
+'\n#Sudo: '
